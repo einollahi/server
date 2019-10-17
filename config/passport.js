5ae285396da41a64d35475f0b08da1994a8e62b4
Alireza Einollahi <einollahi.1366@gmail.com>
2019-10-17 10:09:17 +0330

    cc

--- config/passport.js
+++ config/passport.js
@@ -5,37 +5,37 @@ const bcrypt = require('bcryptjs');
 const User = require('../db/models/user');
 
 passport.serializeUser((user, done) => {
-	return done(null, user.id);
+  return done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-	User.findOne({ where: { id } })
-		.then(user => {
-			if (user) {
-				return done(null, user.id);
-			}
-			return done(null);
-		})
-		.catch(err => {
-			return done(err);
-		});
+  User.findOne({where: {id}})
+    .then(user => {
+      if (user) {
+        return done(null, user.id);
+      }
+      return done(null);
+    })
+    .catch(err => {
+      return done(err);
+    });
 });
 
 passport.use(
-	new LocalStrategy(async (username, password, done) => {
-		try {
-			const user = await User.findOne({ where: { username } });
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({where: {username}});
 
-			if (!user) {
-				return done(null, false, { msg: 'user is not found' });
-			}
-			if (!bcrypt.compareSync(password, user.password)) {
-				return done(null, false, { msg: 'password is not correct' });
-			}
+      if (!user) {
+        return done(null, false, {msg: 'user is not found'});
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, {msg: 'password is not correct'});
+      }
 
-			return done(null, user);
-		} catch (e) {
-			done(err);
-		}
-	})
+      return done(null, user);
+    } catch (e) {
+      done(err);
+    }
+  })
 );
